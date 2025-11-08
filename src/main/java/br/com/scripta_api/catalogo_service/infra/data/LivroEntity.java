89a5dd1f888419a606e19cf81835615f513defa5
miguel.silva <miguelss616@outlook.com.br>
2025-11-08 16:36:56 -0300

    feat: Adiciona Classe entidade de livro
    - Adiciona repository com métodos para buscar por isbne e por nome/autor.
    - Adicionado Optional nos retornos para tratar exceções futuramente.
    
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

---
+++ src/main/java/br/com/scripta_api/catalogo_service/infra/data/LivroEntity.java
@@ -0,0 +1,31 @@
+package br.com.scripta_api.catalogo_service.infra.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity(name = "livros")
+@Table(name = "livros")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LivroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String titulo;
+    private String autor;
+    private Date anoPublicacao;
+    @Column(nullable = false)
+    private Integer quantidadeTotal;
+    @Column(nullable = false)
+    private Integer quantidadeDisponivel;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+}
--- src/main/java/br/com/scripta_api/catalogo_service/infra/gateways/LivroEntityRepository.java
+++ src/main/java/br/com/scripta_api/catalogo_service/infra/gateways/LivroEntityRepository.java
@@ -1,12 +1,20 @@
 package br.com.scripta_api.catalogo_service.infra.gateways;
 
-import br.com.scripta_api.catalogo_service.infra.data.LIvroEntity;
+import br.com.scripta_api.catalogo_service.infra.data.LivroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-/*
-TODO: Adicionar Optional<LivroEntity> findByIsbn(String isbn);.
+import java.util.List;
+import java.util.Optional;
 
-TODO: Adicionar List<LivroEntity> findByTituloContainingOrAutorContaining(String titulo, String autor);.
- */
-public interface LivroEntityRepository extends JpaRepository<LIvroEntity, Long> {
+public interface LivroEntityRepository extends JpaRepository<LivroEntity, Long> {
+
+    Optional<LivroEntity> findByIsbn(String isbn);
+
+    @Query("select e " +
+            "from livros e " +
+            "where upper(e.titulo) like concat('%', upper(:titulo), '%')" +
+            "or upper(e.autor) like concat('%', upper(:autor), '%')")
+    Optional<List<LivroEntity>> findLivroByTituloOrAutor(String titulo, String autor);
+
 }
