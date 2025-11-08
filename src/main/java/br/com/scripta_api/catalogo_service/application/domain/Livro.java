e777a916d229c324e656539b9ea17420002513f1
miguel.silva <miguelss616@outlook.com.br>
2025-11-08 18:57:21 -0300

    feat: Adiciona classe domain LIvro e builder
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/catalogo_service/application/domain/Livro.java
+++ src/main/java/br/com/scripta_api/catalogo_service/application/domain/Livro.java
@@ -1,23 +1,76 @@
 package br.com.scripta_api.catalogo_service.application.domain;
-//
-//TODO: Criar a classe Livro (POJO de domínio, sem anotações JPA, sem Lombok).
-//
-//TODO: Adicionar atributos: Long id, String titulo, String autor, String isbn, int anoPublicacao, int quantidadeTotal, int quantidadeDisponivel.
-//
-//TODO: Criar um construtor de cópia public Livro(Livro livro) (assim como você fez no Usuario.java).
-//
-//TODO: Criar um construtor public Livro() vazio.
-//
-//TODO: Criar getters e setters manuais para todos os campos.
 
 //TODO: Implementar lógica de negócio nos setters ou no build() (ex: quantidadeDisponivel não pode ser negativa).
 
 public class Livro {
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private Integer anoPublicacao;
+    private Integer quantidadeTotal;
+    private Integer quantidadeDsiponivel;
 
+    protected Livro() {
+    }
 
-    // TODO: Criar a classe interna public static class builder para construir o Livro.
-    public static class LivroBuilder {
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Integer getQuantidadeTotal() {
+        return quantidadeTotal;
+    }
+
+    public void setQuantidadeTotal(Integer quantidadeTotal) {
+        this.quantidadeTotal = quantidadeTotal;
+    }
+
+    public Integer getQuantidadeDsiponivel() {
+        return quantidadeDsiponivel;
+    }
+
+    public void setQuantidadeDsiponivel(Integer quantidadeDsiponivel) {
+        if(this.quantidadeDsiponivel == 0) {
+
+        }
+        this.quantidadeDsiponivel = quantidadeDsiponivel;
     }
 
 }
---
+++ src/main/java/br/com/scripta_api/catalogo_service/application/domain/LivroBuilder.java
@@ -0,0 +1,53 @@
+package br.com.scripta_api.catalogo_service.application.domain;
+
+public final class LivroBuilder {
+    private final Livro livro;
+
+    private LivroBuilder() {
+        livro = new Livro();
+    }
+
+    public static LivroBuilder builder() {
+        return new LivroBuilder();
+    }
+
+    public LivroBuilder id(Long id) {
+        livro.setId(id);
+        return this;
+    }
+
+    public LivroBuilder titulo(String titulo) {
+        livro.setTitulo(titulo);
+        return this;
+    }
+
+    public LivroBuilder anoPublicacao(Integer anoPublicacao) {
+        livro.setAnoPublicacao(anoPublicacao);
+        return this;
+    }
+
+    public LivroBuilder quantidadeDisponivel(Integer quantidadeDisponivel) {
+        livro.setQuantidadeDsiponivel(quantidadeDisponivel);
+        return this;
+    }
+
+    public LivroBuilder quantidadeTotal(Integer quantidadeTotal) {
+        livro.setQuantidadeTotal(quantidadeTotal);
+        return this;
+    }
+
+    public LivroBuilder autor(String autor) {
+        livro.setAutor(autor);
+        return this;
+    }
+
+    public LivroBuilder isbn(String isbn) {
+        livro.setIsbn(isbn);
+        return this;
+    }
+
+    public Livro build() {
+        return this.livro;
+    }
+
+}
