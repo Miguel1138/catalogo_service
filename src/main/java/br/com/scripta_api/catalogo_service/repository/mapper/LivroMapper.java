cfe224dce13d3e0c09a70c0abfd71a3bb9845734
miguel.silva <miguelss616@outlook.com.br>
2025-11-22 14:35:31 -0300

    feat: Adiciona classe mapper para conversões de entidade e dominio
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/catalogo_service/repository/mapper/LivroMapper.java
+++ src/main/java/br/com/scripta_api/catalogo_service/repository/mapper/LivroMapper.java
@@ -1,11 +1,36 @@
 package br.com.scripta_api.catalogo_service.repository.mapper;
 
-/*
-TODO: Anotar com @Component.
+import br.com.scripta_api.catalogo_service.application.domain.Livro;
+import br.com.scripta_api.catalogo_service.application.domain.LivroBuilder;
+import br.com.scripta_api.catalogo_service.infra.data.LivroEntity;
+import org.springframework.stereotype.Component;
 
-TODO: Criar public Livro toDomain(LivroEntity entity).
 
-TODO: Criar public LivroEntity toEntity(Livro domain).
- */
+@Component
 public class LivroMapper {
+
+    public static Livro toDomain(LivroEntity entity) {
+        return LivroBuilder.builder()
+                .id(entity.getId())
+                .titulo(entity.getTitulo())
+                .quantidadeTotal(entity.getQuantidadeTotal())
+                .quantidadeDisponivel(entity.getQuantidadeDisponivel())
+                .isbn(entity.getIsbn())
+                .anoPublicacao(entity.getAnoPublicacao())
+                .autor(entity.getAutor())
+                .build();
+    }
+
+    public static LivroEntity toEntity(Livro domain) {
+        return LivroEntity.builder()
+                .id(domain.getId())
+                .titulo(domain.getTitulo())
+                .anoPublicacao(domain.getAnoPublicacao())
+                .autor(domain.getAutor())
+                .isbn(domain.getIsbn())
+                .quantidadeDisponivel(domain.getQuantidadeDsiponivel())
+                .quantidadeTotal(domain.getQuantidadeTotal())
+                .build();
+    }
+
 }
