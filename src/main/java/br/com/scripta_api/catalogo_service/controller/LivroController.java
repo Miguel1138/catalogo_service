a50753dce42eafb07a5aea77675e686e27150165
miguel.silva <miguelss616@outlook.com.br>
2025-11-22 18:11:37 -0300

    feat: Adiciona endpoints da aplicação de catalogo.
    -Validação feita no config de security,
    -Adiciona endpoints internos de incrementar e decrementar para serviço de emprestimo.
    
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/catalogo_service/controller/LivroController.java
+++ src/main/java/br/com/scripta_api/catalogo_service/controller/LivroController.java
@@ -1,29 +1,84 @@
 package br.com.scripta_api.catalogo_service.controller;
 
 /*
-TODO: Anotar com @RestController,
- @RequestMapping("/livros"),
- @RequiredArgsConstructor.
-
-TODO: Injetar private final LivroService livroService; (a interface).
-TODO: Injetar private final IntegracaoService integracaoService;.
-
-TODO: Implementar POST / (protegido):
-  Recebe CriarLivroRequest, converte para Livro (domain) usando o builder,
-  chama livroService.criarLivro() e retorna LivroResponse.fromDomain().
-
-TODO: Implementar
- GET /,
- GET /{id},
- GET /buscar (públicos).
 
 TODO: Implementar
  POST /importar/{isbn} (protegido):
  Chama integracaoService.importarLivroPorIsbn() e retorna LivroResponse.fromDomain().
-
-TODO: Implementar
- PUT /{id}/estoque/decrementar e
- .../incrementar (protegidos).
  */
+
+import br.com.scripta_api.catalogo_service.api.gateways.GoogleBooksApiService;
+import br.com.scripta_api.catalogo_service.application.domain.LivroBuilder;
+import br.com.scripta_api.catalogo_service.application.gateways.service.LivroService;
+import br.com.scripta_api.catalogo_service.dtos.CriarLivroRequest;
+import br.com.scripta_api.catalogo_service.dtos.LivroResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/livros")
+@RequiredArgsConstructor
 public class LivroController {
+    private final LivroService livroService;
+    private final GoogleBooksApiService apiService;
+
+    @PostMapping
+    public LivroResponse criarLivro(CriarLivroRequest request) {
+        return LivroResponse.fromDomain(livroService.criarLivro(
+                        LivroBuilder.builder()
+                                .titulo(request.getTitulo())
+                                .autor(request.getAutor())
+                                .isbn(request.getIsbn())
+                                .anoPublicacao(request.getAnoPublicacao())
+                                .quantidadeTotal(request.getQuantidadeTotal())
+                                .quantidadeDisponivel(request.getQuantidadeDsiponivel())
+                                .build()
+                )
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroResponse>> listarLivros() {
+        return ResponseEntity.ok(livroService.listarLivros().stream()
+                .map(LivroResponse::fromDomain)
+                .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LivroResponse> buscarPorId(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(LivroResponse.fromDomain(livroService.buscarPorId(id)));
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<LivroResponse>> buscarLivrosPorPalavra(@RequestBody String palavra) {
+        return ResponseEntity.ok(
+                livroService.buscar(palavra).stream()
+                        .map(LivroResponse::fromDomain)
+                        .toList()
+        );
+    }
+
+    @PutMapping("/{id}/estoque/incrementar")
+    public ResponseEntity<LivroResponse> incrementarEstoque(@Valid @PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                LivroResponse.fromDomain(livroService.incrementarEstoque(id))
+        );
+    }
+
+    @PutMapping("/{id}/estoque/decrementar")
+    public ResponseEntity<LivroResponse> decrementarEstoque(@Valid @PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                LivroResponse.fromDomain(livroService.decrementarEstoque(id))
+        );
+    }
+
+    @PostMapping("/importar/{isbn}")
+    public ResponseEntity<LivroResponse> importaLivro(@PathVariable String isbn) {
+        return ResponseEntity.ok(LivroResponse.fromDomain(apiService.importarLivroPorIsbn(isbn)));
+    }
+
 }
